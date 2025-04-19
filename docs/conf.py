028c48e59800c9f3bcdea0111d9c3b2651a85819
Marco Elste <marco.elste@web.de>
2025-04-19 19:02:06 +0200

    Updated conf.py: Extentions implemented, rtd_theme used

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,10 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sys, os
+
+sys.path.insert(0, os.path.abspath('../src'))
+
 project = 'Teach-App'
 copyright = '2025, Marco Elste'
 author = 'Marco Elste'
@@ -14,7 +18,12 @@ release = '1.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx_autodoc_typehints',
+    'sphinxcontrib.httpdomain',
+    'myst_parser'
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
@@ -24,5 +33,5 @@ language = 'de'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
