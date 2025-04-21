ecb798cda478040f0f77f130ffd1e2c1d1e14ab2
Marco Elste <marco.elste@web.de>
2025-04-21 09:51:51 +0200

    Updated tables.sql: Student

--- src/database/tables.sql
+++ src/database/tables.sql
@@ -17,3 +17,9 @@ CREATE TABLE grade(
     grd_name VARCHAR(20) NOT NULL PRIMARY KEY,
     teach_id INTEGER NOT NULL REFERENCES teacher(teach_id)
 );
+CREATE TABLE student(
+    std_id INTEGER NOT NULL PRIMARY KEY,
+    std_first_name VARCHAR(20) NOT NULL,
+    std_last_name VARCHAR(20) NOT NULL,
+    grd_name VARCHAR(20) REFERENCES grade(grd_name) ON UPDATE CASCADE ON DELETE SET NULL
+);
