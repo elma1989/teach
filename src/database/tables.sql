787b2da7fa50e0a7cd509b55d8f650903e2613e3
Marco Elste <marco.elste@web.de>
2025-04-21 10:25:10 +0200

    Updated tables.sql: Lesson

--- src/database/tables.sql
+++ src/database/tables.sql
@@ -33,3 +33,24 @@ CREATE TABLE student_course(
     crs_name VARCHAR(20) NOT NULL REFERENCES course(crs_name) ON DELETE CASCADE,
     PRIMARY KEY (std_id, crs_name)
 );
+CREATE TABLE lesson(
+    crs_name VARCHAR(20) NOT NULL REFERENCES course(crs_name) ON DELETE CASCADE,
+    les_time DATETIME NOT NULL,
+    les_topic TEXT,
+    PRIMARY KEY (crs_name, les_time)
+);
+CREATE TABLE lesson_homework(
+    crs_name VARCHAR(20) NOT NULL,
+    les_time DATETIME NOT NULL,
+    les_homework TEXT NOT NULL,
+    PRIMARY KEY (crs_name, les_time, les_homework),
+    FOREIGN KEY (crs_name, les_time) REFERENCES lesson(crs_name, les_time)
+);
+CREATE TABLE lesson_student(
+    crs_name VARCHAR(20) NOT NULL,
+    les_time DATETIME NOT NULL,
+    std_id INTEGER NOT NULL REFERENCES student (std_id),
+    les_student_present BOOLEAN NOT NULL DEFAULT false,
+    PRIMARY KEY (crs_name, les_time, std_id),
+    FOREIGN KEY (crs_name, les_time) REFERENCES lesson(crs_name, les_time)
+);
