5b0faad6490809871948ffda48113c626a74465e
Marco Elste <marco.elste@web.de>
2025-04-21 10:07:49 +0200

    Updated tables.sql: Course

--- src/database/tables.sql
+++ src/database/tables.sql
@@ -9,8 +9,8 @@ CREATE TABLE teacher(
     teach_birth_date DATE NOT NULL
 );
 CREATE TABLE teacher_subject(
-    teach_id INTEGER NOT NULL REFERENCES teacher(teach_id),
-    sub_abr CHAR(3) NOT NULL REFERENCES subject(sub_abr),
+    teach_id INTEGER NOT NULL REFERENCES teacher(teach_id) ON DELETE CASCADE,
+    sub_abr CHAR(3) NOT NULL REFERENCES subject(sub_abr) ON DELETE CASCADE,
     PRIMARY KEY (teach_id, sub_abr)
 );
 CREATE TABLE grade(
@@ -22,4 +22,14 @@ CREATE TABLE student(
     std_first_name VARCHAR(20) NOT NULL,
     std_last_name VARCHAR(20) NOT NULL,
     grd_name VARCHAR(20) REFERENCES grade(grd_name) ON UPDATE CASCADE ON DELETE SET NULL
+);
+CREATE TABLE course(
+    crs_name VARCHAR(20) NOT NULL PRIMARY KEY,
+    teach_id INTEGER NOT NULL REFERENCES teacher(teach_id),
+    sub_abr CHAR(3) NOT NULL REFERENCES subject(sub_abr)
+);
+CREATE TABLE student_course(
+    std_id INTEGER NOT NULL REFERENCES student(std_id) ON DELETE CASCADE,
+    crs_name VARCHAR(20) NOT NULL REFERENCES course(crs_name) ON DELETE CASCADE,
+    PRIMARY KEY (std_id, crs_name)
 );
