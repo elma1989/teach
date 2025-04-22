7dfc321364722e9c1eb5b3334ddd3593b3310306
Marco Elste <marco.elste@web.de>
2025-04-22 08:12:35 +0200

    Updated subject.py: Subject.exists()

--- src/database/subject.py
+++ src/database/subject.py
@@ -1,5 +1,5 @@
 import re
-from database import DataObject
+from database import DataObject, Error
 
 class Subject(DataObject):
     """
@@ -12,8 +12,8 @@ class Subject(DataObject):
         self.__abr:str = ''
         self.__name:str = ''
 
-        re_abr = r'[A-Za-z]+'
-        re_name = r'[A-Z][a-z]+'
+        re_abr:str = r'[A-Za-z]+'
+        re_name:str = r'[A-Z][a-z]+'
 
         match_abr:list[str] = re.findall(re_abr, abr)
         match_name:list[str] = re.findall(re_name, name)
@@ -32,3 +32,29 @@ class Subject(DataObject):
     def name(self) -> str:
         ''':getter: Liefert die Langbezeichnung des Faches'''
         return self.__name
+
+    def __repr__(self) -> str: return self.abr
+
+    def __eq__(self, other) -> bool:
+        if not isinstance(other, Subject): return False
+        return self.abr == other.abr
+
+    def exists(self) -> bool:
+        """
+        Prüft, ob das Unterrichtfach bereits existiert.
+
+        :return: **True**, wenn das Fach bereits vorhanden ist
+        """
+        sql:str = 'SELECT * FROM subject WHERE sub_abr = ?'
+        success:bool = False
+
+        try:
+            self.connect()
+            if self.con and self.c:
+                self.c.execute(sql, (self.abr,))
+                res = self.c.fetchone()
+                if res: success = True
+        except Error as e: print(e)
+        finally: self.close()
+
+        return success
