b799ed22b83a462dc5ec543ef5b202b961cfac3e
Marco Elste <marco.elste@web.de>
2025-04-24 10:24:52 +0200

    Updated database/teacher.py: Teacher.subjects

--- src/database/teacher.py
+++ src/database/teacher.py
@@ -11,7 +11,7 @@ class Teacher(Person):
     """
     def __init__(self, fname:str, lname:str, birth_date:str, id:int = 0) -> None:
         super().__init__(fname, lname, birth_date, id)
-        self.__subject:list[Subject] = []
+        self.__subjects:list[Subject] = []
         sql:str = """SELECT teach_id FROM teacher
             WHERE teach_first_name = ? AND teach_last_name = ? AND teach_birth_date = ?"""
         
@@ -24,3 +24,27 @@ class Teacher(Person):
                     if res: self.id = res[0]
             except Error as e: print(e)
             finally: self.close()
+
+    def subjects(self) -> list[Subject]:
+        """
+        Liefert alle Fächer eines Lehrers.
+
+        :return: Fächerliste
+        """
+        sql:str = """SELECT s.sub_abr, s.sub_name
+            FROM subject s JOIN teacher_subject ts ON t.sub_abr = ts.sub_abr
+            WHERE ts.teach_id = ?
+            ORDER BY s.sub_abr"""
+        
+        if self.id == 0: return []
+
+        try:
+            self.connect()
+            if self.con and self.c:
+                self.c.execute(sql, (self.id,))
+                res = self.c.fetchall()
+                self.__subjects = [Subject(row[0], row[1]) for row in res]
+        except Error as e: print(e)
+        finally: self.close()
+
+        return self.__subjects
