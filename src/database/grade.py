94e07ad8809e7dcf91f6ec3c7daba633793f7bfa
Marco Elste <marco.elste@web.de>
2025-04-24 18:41:36 +0200

    Updated database/grade.py: Grade.name

--- src/database/grade.py
+++ src/database/grade.py
@@ -1,4 +1,4 @@
-from database import DataObject, Teacher, Error
+from database import DataObject, Teacher, Error, FKON
 
 class Grade(DataObject):
     """
@@ -10,7 +10,7 @@ class Grade(DataObject):
     .. note:: Es ist zu emphehlen, dass bei jährlich wechselden Klassennamen die Klassenstufen unter 10 mit einer führenden 0 
         z. B. 09a zu beginnen, da eine alphabetische Soritierung bei Klassenlisten vorgenommen wird.
     """
-    def __init__(self, name:str, leader:Teacher|None = None):
+    def __init__(self, name:str, leader:Teacher|None = None) -> None:
         super().__init__()
         self.__name:str = name
         self.__leader:Teacher|None = None
@@ -26,3 +26,28 @@ class Grade(DataObject):
                     res = self.c.fetchone()
                     if res: self.__leader = Teacher(res[0],res[1],res[2],res[3])
             except Error as e: print(e)
+    
+    @property
+    def name(self) -> str:
+        """
+        Verwatet den Klassennamen.
+
+        :getter: Liefert den Namen der Klasse
+        :setter: Ändert den Klassennamen
+        """
+        return self.__name
+    
+    @name.setter
+    def name(self,name:str) -> None:
+        sql:str = """UPDADE grade
+            SET grd_name = ?
+            WHERE grd_name = ?"""
+        
+        try:
+            self.connect()
+            if self.con and self.c:
+                self.c.execute(FKON)
+                self.c.execute(sql, (name, self.name))
+                self.con.commit()
+        except Error as e: print(e)
+        finally: self.close()
