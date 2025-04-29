f335446cef5b1ee4307cacc6c04b207404d144e7
Marco Elste <marco.elste@web.de>
2025-04-29 10:23:41 +0200

    Updated database/lesson.py: Lesson.homeworks

--- src/database/lesson.py
+++ src/database/lesson.py
@@ -7,6 +7,7 @@ class Lesson(DataObject):
 
     :param course: Zugehöriger Kurs
     :param time: Geplanter Unterrichtsbeginn (JJJJ-TT-MM HH:MM)
+    :param topic: Thema der Stunde (Standard: "")
     """
     def __init__(self, course:Course, time:str, topic:str = '') -> None:
         self.__course:Course|None = course if isinstance(course, Course) and course.exists() else None
@@ -83,3 +84,21 @@ class Lesson(DataObject):
                     self.con.commit()
             except Error as e: print(e)
             finally: self.close()
+
+    @property
+    def homeworks(self) -> list[str]:
+        ''':getter: Liefert ale zu der Stunde aufgetragenen Hausaufgaben'''
+        sql:str = """SELECT les_homework FROM lesson_homework
+            WHERE crs_name = ? AND les_time = ?"""
+        
+        if isinstance(self.course, Course) and self.time:
+            try:
+                self.connect()
+                if self.con and self.c:
+                    self.c.execute(sql,(self.course.name, self.db_time))
+                    res = self.c.fetchall()
+                    self.__homeworks = [row[0] for row in res]
+            except Error as e: print(e)
+            finally: self.close()
+
+        return self.__homeworks
