f8dc37719d1acb907e40f18920198f51eb76d5c7
Marco Elste <marco.elste@web.de>
2025-05-02 18:06:39 +0200

    Added route/teacher.py

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from route import site, subject
+from route import site, subject, teacher
 
 app = Flask(__name__)
 app.register_blueprint(site)
 app.register_blueprint(subject)
+app.register_blueprint(teacher)
--- src/route/__init__.py
+++ src/route/__init__.py
@@ -1,2 +1,3 @@
 from .site import site_bp as site
 from .subject import subject_bp as subject
+from .teacher import teacher_bp as teacher
