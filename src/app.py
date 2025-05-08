9eca3c81e357a1fa67f79453d270429a60a660ea
Marco Elste <marco.elste@web.de>
2025-05-08 15:36:39 +0200

    Added route/grade.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from route import subject, teacher
+from route import subject, teacher, grade
 
 app = Flask(__name__)
 
 app.register_blueprint(subject)
 app.register_blueprint(teacher)
+app.register_blueprint(grade)
--- src/route/__init__.py
+++ src/route/__init__.py
@@ -1,2 +1,3 @@
 from .subject import subject_bp as subject
 from .teacher import teacher_bp as teacher
+from .grade import grade_bp as grade
